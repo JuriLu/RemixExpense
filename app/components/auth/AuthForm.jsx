570a14db50a30d6fef1aa57e4af87b2590e05d6f
Juri <lubonjajurgen1@gmail.com>
2023-01-07 10:00:57 +0100

    Separating login from signup with query params

--- app/components/auth/AuthForm.jsx
+++ app/components/auth/AuthForm.jsx
@@ -1,4 +1,4 @@
-import { Link, useSearchParams } from '@remix-run/react';
+import { Form, Link, useSearchParams } from '@remix-run/react';
 import { FaLock, FaUserPlus } from 'react-icons/fa';
 
 export default function AuthForm() {
@@ -10,7 +10,7 @@ export default function AuthForm() {
   const toggleBtnCaption = authMode === 'login' ? 'Create a new user' : 'Log in with existing user'
 
   return (
-    <form method="post" className="form" id="auth-form">
+    <Form method="post" className="form" id="auth-form">
       <div className="icon-img">
         {authMode === 'login' ?  <FaLock /> : <FaUserPlus/> }
       </div>
@@ -26,7 +26,7 @@ export default function AuthForm() {
         <button>{submitBtnCaption}</button>
         <Link to={authMode === 'login' ? '?mode=signup' : '?mode=login'}>{toggleBtnCaption}</Link>
       </div>
-    </form>
+    </Form>
   );
 }
 
--- app/routes/auth.jsx
+++ app/routes/auth.jsx
@@ -5,6 +5,21 @@ export default function AuthPage() {
   return <AuthForm />;
 }
 
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get("mode") || "login";
+
+  const formData = await request.formData();
+  const credentials = Object.fromEntries(formData);
+  console.log(credentials);
+
+  if (authMode === "login") {
+    //login Logic
+  } else {
+    //signup Logic
+  }
+}
+
 export function links() {
   return [{ rel: "stylesheet", href: authStyles }];
 }
