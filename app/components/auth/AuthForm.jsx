90a164e27f7952dfac7ed72dc2e2293519e91000
Juri <lubonjajurgen1@gmail.com>
2023-01-07 10:29:07 +0100

    Formatting + Home Link Added to auth

--- app/components/auth/AuthForm.jsx
+++ app/components/auth/AuthForm.jsx
@@ -1,20 +1,21 @@
 import { Form, Link, useSearchParams, useTransition as useNavigation } from '@remix-run/react';
-import { FaLock, FaUserPlus } from 'react-icons/fa';
+import { FaHome, FaLock, FaUserPlus } from "react-icons/fa";
 
 export default function AuthForm() {
-  const [searchParams,setSearchParams] =useSearchParams() //Remix Hook
-  const navigation = useNavigation()
+  const [searchParams, setSearchParams] = useSearchParams(); //Remix Hook
+  const navigation = useNavigation();
 
-  const authMode = searchParams.get('mode') || 'login'  // in case the mode doesn't exists we set auto to login
+  const authMode = searchParams.get("mode") || "login"; // in case the mode doesn't exists we set auto to login
 
-  const submitBtnCaption = authMode === 'login' ? 'Login' : 'Create User'
-  const toggleBtnCaption = authMode === 'login' ? 'Create a new user' : 'Log in with existing user'
+  const submitBtnCaption = authMode === "login" ? "Login" : "Create User";
+  const toggleBtnCaption =
+    authMode === "login" ? "Create a new user" : "Log in with existing user";
 
-  const isSubmitting = navigation.state !== "idle"
+  const isSubmitting = navigation.state !== "idle";
   return (
     <Form method="post" className="form" id="auth-form">
       <div className="icon-img">
-        {authMode === 'login' ?  <FaLock /> : <FaUserPlus/> }
+        {authMode === "login" ? <FaLock /> : <FaUserPlus />}
       </div>
       <p>
         <label htmlFor="email">Email Address</label>
@@ -25,11 +26,18 @@ export default function AuthForm() {
         <input type="password" id="password" name="password" minLength={7} />
       </p>
       <div className="form-actions">
-        <button 
-        disabled={isSubmitting}>
-          {isSubmitting? 'Authenticating...' : submitBtnCaption}
-          </button>
-        <Link to={authMode === 'login' ? '?mode=signup' : '?mode=login'}>{toggleBtnCaption}</Link>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Authenticating..." : submitBtnCaption}
+        </button>
+        <Link to={authMode === "login" ? "?mode=signup" : "?mode=login"}>
+          {toggleBtnCaption}
+        </Link>
+        <Link to="/">
+          <span className="homeSpan">
+            Home
+            <FaHome className="homeIconAuth" />
+          </span>
+        </Link>
       </div>
     </Form>
   );
