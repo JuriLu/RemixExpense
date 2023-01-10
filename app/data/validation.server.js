7ca6d139c7504c954186c8ef0cbdc1759f639f22
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2023-01-10 15:47:14 +0100

    Adding auth validation also

--- app/data/validation.server.js
+++ app/data/validation.server.js
@@ -30,3 +30,27 @@ function isValidTitle(value) {
        throw validationErrors;
      }
    }
+
+   function isValidEmail(value) {
+    return value && value.includes('@');
+  }
+  
+  function isValidPassword(value) {
+    return value && value.trim().length >= 7;
+  }
+  
+  export function validateCredentials(input) {
+    let validationErrors = {};
+  
+    if (!isValidEmail(input.email)) {
+      validationErrors.email = 'Invalid email address.'
+    }
+  
+    if (!isValidPassword(input.password)) {
+      validationErrors.password = 'Invalid password. Must be at least 7 characters long.'
+    }
+  
+    if (Object.keys(validationErrors).length > 0) {
+      throw validationErrors;
+    }
+  }
